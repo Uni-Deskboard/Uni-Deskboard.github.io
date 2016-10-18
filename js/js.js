950d184e21a3dff92f62a54abde6994740ece3f2
Vincent Harvey <vharv008@uottawa.ca>
2016-10-18 16:39:03 -0400

    Mon accueil + nouveau menu

---
+++ js/js.js
@@ -0,0 +1,29 @@
+$(document).ready(main);
+
+var i = 1;
+
+function main() {
+	$('.menu_bar').click(function(){
+		//$('nav').toggle();
+
+		if(i == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			i = 0;
+		} else {
+			i = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+}
+
+$(document).on('click', '#scroll', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+});
